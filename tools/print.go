b6a694e4867049b4cea3d6b4f1eb288e94f333bf
broothie <adbooth8@gmail.com>
2023-03-03 15:56:38 -0800

    update table padding

--- tools/print.go
+++ tools/print.go
@@ -26,7 +26,7 @@ func (t Tools) Print() error {
 	}
 
 	rows = append([]string{header}, rows...)
-	table := tabwriter.NewWriter(os.Stdout, 0, 0, 3, ' ', 0)
+	table := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', 0)
 	if _, err := fmt.Fprintln(table, strings.Join(rows, "\n")); err != nil {
 		return errors.Wrap(err, "failed to write rows to table")
 	}
@@ -50,7 +50,7 @@ func (t Tasks) Print() error {
 	sort.Strings(rows)
 	rows = append([]string{header}, rows...)
 
-	table := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', 0)
+	table := tabwriter.NewWriter(os.Stdout, 0, 0, 3, ' ', 0)
 	if _, err := fmt.Fprintln(table, strings.Join(rows, "\n")); err != nil {
 		return errors.Wrap(err, "failed to write rows to table")
 	}
