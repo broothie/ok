04710f4e4a89e19d9b4bd40b3b21e98fd91111bb
Andrew Booth <adbooth8@gmail.com>
2026-01-25 11:57:38 -0800

    refactor: move tool processing logic to processTool method

--- set_up_tools.go
+++ set_up_tools.go
@@ -16,33 +16,37 @@ func (o *Ok) SetUpTools(ctx context.Context, tls []Tool) error {
 	group, ctx := errgroup.WithContext(ctx)
 
 	for _, tl := range tls {
-		group.Go(func() error {
-			commandPath, commandPathErr := exec.LookPath(tl.CommandName)
-
-			var filePaths []string
-			var filePathErrs []error
-			for _, fileGlob := range tl.FileGlobs {
-				globFilePaths, err := doublestar.Glob(os.DirFS("."), fileGlob)
-
-				filePaths = append(filePaths, globFilePaths...)
-				filePathErrs = append(filePathErrs, err)
-			}
-
-			filePaths = lo.UniqBy(filePaths, func(filePath string) string { return strings.ToLower(filePath) })
-			filePathsErr := errors.Join(filePathErrs...)
-
-			o.toolsLock.Lock()
-			o.tools = append(o.tools, toolInfo{
-				Tool:           tl,
-				CommandPath:    commandPath,
-				CommandPathErr: commandPathErr,
-				FilePaths:      filePaths,
-				FilePathsErr:   filePathsErr,
-			})
-			o.toolsLock.Unlock()
-			return nil
-		})
+		group.Go(o.processTool(tl))
 	}
 
 	return group.Wait()
+}
+
+func (o *Ok) processTool(tl Tool) func() error {
+	return func() error {
+		commandPath, commandPathErr := exec.LookPath(tl.CommandName)
+
+		var filePaths []string
+		var filePathErrs []error
+		for _, fileGlob := range tl.FileGlobs {
+			globFilePaths, err := doublestar.Glob(os.DirFS("."), fileGlob)
+
+			filePaths = append(filePaths, globFilePaths...)
+			filePathErrs = append(filePathErrs, err)
+		}
+
+		filePaths = lo.UniqBy(filePaths, func(filePath string) string { return strings.ToLower(filePath) })
+		filePathsErr := errors.Join(filePathErrs...)
+
+		o.toolsLock.Lock()
+		o.tools = append(o.tools, toolInfo{
+			Tool:           tl,
+			CommandPath:    commandPath,
+			CommandPathErr: commandPathErr,
+			FilePaths:      filePaths,
+			FilePathsErr:   filePathsErr,
+		})
+		o.toolsLock.Unlock()
+		return nil
+	}
 }
