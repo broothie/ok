85d6e5ef28ce9a971d33f4ea6e01edb96652c0c2
Andrew Booth <adbooth8@gmail.com>
2026-01-25 14:53:44 -0800

    fix tests

--- cli/parse_test.go
+++ cli/parse_test.go
@@ -21,79 +21,158 @@ func TestArgParser_Parse(t *testing.T) {
 	flagCasesBuilder := func() map[string]FlagCase {
 		return map[string]FlagCase{
 			"string long": {
-				flag:          &Flag{Name: "flag-a", Type: FlagTypeString},
+				flag: &Flag{
+					Name:         "flag-a",
+					Type:         FlagTypeString,
+					DefaultValue: "",
+				},
 				tokens:        []string{"--flag-a", "flag-a-value"},
 				expectedValue: "flag-a-value",
 			},
 			"string alias": {
-				flag:          &Flag{Name: "flag-b", Type: FlagTypeString, Aliases: []string{"flag-b-alias"}},
+				flag: &Flag{
+					Name:         "flag-b",
+					Type:         FlagTypeString,
+					Aliases:      []string{"flag-b-alias"},
+					DefaultValue: "",
+				},
 				tokens:        []string{"--flag-b-alias", "flag-b-value"},
 				expectedValue: "flag-b-value",
 			},
 			"string short": {
-				flag:          &Flag{Name: "flag-c", Type: FlagTypeString, Shorts: []rune{'c'}},
+				flag: &Flag{
+					Name:         "flag-c",
+					Type:         FlagTypeString,
+					Shorts:       []rune{'c'},
+					DefaultValue: "",
+				},
 				tokens:        []string{"-c", "flag-c-value"},
 				expectedValue: "flag-c-value",
 			},
 			"string long equals": {
-				flag:          &Flag{Name: "flag-d", Type: FlagTypeString},
+				flag: &Flag{
+					Name:         "flag-d",
+					Type:         FlagTypeString,
+					DefaultValue: "",
+				},
 				tokens:        []string{"--flag-d=flag-d-value"},
 				expectedValue: "flag-d-value",
 			},
 			"string alias equals": {
-				flag:          &Flag{Name: "flag-e", Type: FlagTypeString, Aliases: []string{"flag-e-alias"}},
+				flag: &Flag{
+					Name:         "flag-e",
+					Type:         FlagTypeString,
+					Aliases:      []string{"flag-e-alias"},
+					DefaultValue: "",
+				},
 				tokens:        []string{"--flag-e-alias=flag-e-value"},
 				expectedValue: "flag-e-value",
 			},
 			"string short equals": {
-				flag:          &Flag{Name: "flag-f", Type: FlagTypeString, Shorts: []rune{'f'}},
+				flag: &Flag{
+					Name:         "flag-f",
+					Type:         FlagTypeString,
+					Shorts:       []rune{'f'},
+					DefaultValue: "",
+				},
 				tokens:        []string{"-f=flag-f-value"},
 				expectedValue: "flag-f-value",
 			},
 			"bool long": {
-				flag:          &Flag{Name: "flag-g", Type: FlagTypeBool},
+				flag: &Flag{
+					Name:         "flag-g",
+					Type:         FlagTypeBool,
+					DefaultValue: false,
+				},
 				tokens:        []string{"--flag-g"},
 				expectedValue: true,
 			},
 			"bool alias": {
-				flag:          &Flag{Name: "flag-h", Type: FlagTypeBool, Aliases: []string{"flag-h-alias"}},
+				flag: &Flag{
+					Name:         "flag-h",
+					Type:         FlagTypeBool,
+					Aliases:      []string{"flag-h-alias"},
+					DefaultValue: false,
+				},
 				tokens:        []string{"--flag-h-alias"},
 				expectedValue: true,
 			},
 			"bool short": {
-				flag:          &Flag{Name: "flag-i", Type: FlagTypeBool, Shorts: []rune{'i'}},
+				flag: &Flag{
+					Name:         "flag-i",
+					Type:         FlagTypeBool,
+					Shorts:       []rune{'i'},
+					DefaultValue: false,
+				},
 				tokens:        []string{"-i"},
 				expectedValue: true,
 			},
 			"bool long equals false": {
-				flag:          &Flag{Name: "flag-j", Type: FlagTypeBool},
+				flag: &Flag{
+					Name:         "flag-j",
+					Type:         FlagTypeBool,
+					DefaultValue: false,
+				},
 				tokens:        []string{"--flag-j=false"},
 				expectedValue: false,
 			},
 			"bool long equals true": {
-				flag:          &Flag{Name: "flag-k", Type: FlagTypeBool},
+				flag: &Flag{
+					Name:         "flag-k",
+					Type:         FlagTypeBool,
+					DefaultValue: false,
+				},
 				tokens:        []string{"--flag-k=true"},
 				expectedValue: true,
 			},
 			"bool alias equals false": {
-				flag:          &Flag{Name: "flag-l", Type: FlagTypeBool, Aliases: []string{"flag-l-alias"}},
+				flag: &Flag{
+					Name:         "flag-l",
+					Type:         FlagTypeBool,
+					Aliases:      []string{"flag-l-alias"},
+					DefaultValue: false,
+				},
 				tokens:        []string{"--flag-l-alias=0"},
 				expectedValue: false,
 			},
 			"bool alias equals true": {
-				flag:          &Flag{Name: "flag-m", Type: FlagTypeBool, Aliases: []string{"flag-m-alias"}},
+				flag: &Flag{
+					Name:         "flag-m",
+					Type:         FlagTypeBool,
+					Aliases:      []string{"flag-m-alias"},
+					DefaultValue: false,
+				},
 				tokens:        []string{"--flag-m-alias=1"},
 				expectedValue: true,
 			},
 			"bool short equals false": {
-				flag:          &Flag{Name: "flag-n", Type: FlagTypeBool, Shorts: []rune{'n'}},
+				flag: &Flag{
+					Name:         "flag-n",
+					Type:         FlagTypeBool,
+					Shorts:       []rune{'n'},
+					DefaultValue: false,
+				},
 				tokens:        []string{"-n=f"},
 				expectedValue: false,
 			},
 			"bool short equals true": {
-				flag:          &Flag{Name: "flag-o", Type: FlagTypeBool, Shorts: []rune{'o'}},
+				flag: &Flag{
+					Name:         "flag-o",
+					Type:         FlagTypeBool,
+					Shorts:       []rune{'o'},
+					DefaultValue: false,
+				},
 				tokens:        []string{"-o=TRUE"},
 				expectedValue: true,
+			},
+			"bool long invert": {
+				flag: &Flag{
+					Name:         "flag-p",
+					Type:         FlagTypeBool,
+					DefaultValue: true,
+				},
+				tokens:        []string{"--flag-p"},
+				expectedValue: false,
 			},
 		}
 	}
