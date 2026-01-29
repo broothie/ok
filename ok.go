e755511c5ec8d17e7175092efdeecef99652705c
Andrew Booth <adbooth8@gmail.com>
2026-01-29 11:27:50 -0800

    shifting methods around

--- ok.go
+++ ok.go
@@ -1,13 +1,7 @@
 package ok
 
 import (
-	"context"
-	"os"
 	"sync"
-
-	"github.com/bobg/errors"
-	"github.com/broothie/cob"
-	"github.com/samber/lo"
 )
 
 type Ok struct {
@@ -22,35 +16,4 @@ func New() *Ok {
 		toolsLock: new(sync.Mutex),
 		tasksLock: new(sync.Mutex),
 	}
-}
-
-func (o *Ok) RunTask(ctx context.Context, taskName string, remainingArgs []string) error {
-	tsk, found := lo.Find(o.tasks, func(tsk taskInfo) bool { return tsk.Name == taskName })
-	if !found {
-		return errors.Errorf("no task found with name %q", taskName)
-	}
-
-	return tsk.Run(ctx, remainingArgs)
-}
-
-func (i taskInfo) Run(ctx context.Context, remainingArgs []string) error {
-	toolCfg := ToolConfig{
-		Executable: i.tool.commandPath,
-	}
-	options, err := i.RunOptions(ctx, remainingArgs, toolCfg)
-	if err != nil {
-		return errors.Wrapf(err, "getting run options for %s task %q", i.tool.Name, i.Name)
-	}
-
-	options = append(options,
-		cob.AddEnv("OK_TASK", i.Task.Name),
-		cob.AddEnv("OK_TOOL", i.tool.Name),
-		cob.AddEnv("OK_VERSION", Version()),
-		cob.SetStdin(os.Stdin),
-		cob.SetStdout(os.Stdout),
-		cob.SetStderr(os.Stderr),
-	)
-
-	_, err = cob.Run(ctx, i.tool.commandPath, options...)
-	return errors.Wrapf(err, "running %s task %q", i.tool.Name, i.Name)
 }
--- task.go
+++ task.go
@@ -4,10 +4,12 @@ import (
 	"context"
 	"io"
 	"log/slog"
+	"os"
 	"os/exec"
 	"sort"
 
 	"github.com/bobg/errors"
+	"github.com/broothie/cob"
 	"github.com/broothie/ok/table"
 	"github.com/broothie/option"
 	"github.com/samber/lo"
@@ -23,6 +25,27 @@ type taskInfo struct {
 	Task
 	tool     *toolInfo
 	filePath string
+}
+
+func (i taskInfo) Run(ctx context.Context, remainingArgs []string) error {
+	options, err := i.RunOptions(ctx, remainingArgs, ToolConfig{
+		Executable: i.tool.commandPath,
+	})
+	if err != nil {
+		return errors.Wrapf(err, "getting run options for %s task %q", i.tool.Name, i.Name)
+	}
+
+	options = append(options,
+		cob.AddEnv("OK_TASK", i.Task.Name),
+		cob.AddEnv("OK_TOOL", i.tool.Name),
+		cob.AddEnv("OK_VERSION", Version()),
+		cob.SetStdin(os.Stdin),
+		cob.SetStdout(os.Stdout),
+		cob.SetStderr(os.Stderr),
+	)
+
+	_, err = cob.Run(ctx, i.tool.commandPath, options...)
+	return errors.Wrapf(err, "running %s task %q", i.tool.Name, i.Name)
 }
 
 func (o *Ok) ListTasks(w io.Writer) error {
@@ -76,4 +99,13 @@ func (o *Ok) processFile(ctx context.Context, tl toolInfo, filePath string) func
 		o.tasksLock.Unlock()
 		return nil
 	}
+}
+
+func (o *Ok) RunTask(ctx context.Context, taskName string, remainingArgs []string) error {
+	tsk, found := lo.Find(o.tasks, func(tsk taskInfo) bool { return tsk.Name == taskName })
+	if !found {
+		return errors.Errorf("no task found with name %q", taskName)
+	}
+
+	return tsk.Run(ctx, remainingArgs)
 }
